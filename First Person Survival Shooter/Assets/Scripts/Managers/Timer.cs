8c6c9071d9383d23b6536a43e2e79f2bf62bd3cb
Zariay <alexswfan@gmail.com>
2017-05-25 13:58:09 -0700

    ZombunnySpawn

---
+++ First Person Survival Shooter/Assets/Scripts/Managers/Timer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    float time;
+    // Update is called once per frame
+    void Update()
+    {
+        time = Time.deltaTime;
+        timerText.text = time.ToString();
+    }
+}
