f7e823e7a5d3a28083c6b674d53caa3db3e0633c
Zariay <alexswfan@gmail.com>
2017-05-25 13:37:14 -0700

    Minimap Complete

---
+++ First Person Survival Shooter/Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+    }
+}
